3209c8ba47bbe8d571bbf9bf0ae5a9d3c804ee8f
ghousemohamed <ghousemohamed7126@gmail.com>
2020-06-18 14:23:52 +0530

    init

---
+++ lib/test_web/live/page_live.ex
@@ -0,0 +1,46 @@
+defmodule TestWeb.PageLive do
+  use TestWeb, :live_view
+
+  @impl true
+  def mount(_params, _session, socket) do
+    send(self(), {:test, 1})
+    {:ok, assign(socket, query: "", results: %{})}
+  end
+
+  def handle_info({:test, params}, socket) do
+    IO.inspect params
+    Process.send_after(self(), {:test, params+1}, 100)
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_event("suggest", %{"q" => query}, socket) do
+    {:noreply, assign(socket, results: search(query), query: query)}
+  end
+
+  @impl true
+  def handle_event("search", %{"q" => query}, socket) do
+    case search(query) do
+      %{^query => vsn} ->
+        {:noreply, redirect(socket, external: "https://hexdocs.pm/#{query}/#{vsn}")}
+
+      _ ->
+        {:noreply,
+         socket
+         |> put_flash(:error, "No dependencies found matching \"#{query}\"")
+         |> assign(results: %{}, query: query)}
+    end
+  end
+
+  defp search(query) do
+    if not TestWeb.Endpoint.config(:code_reloader) do
+      raise "action disabled when not in development"
+    end
+
+    for {app, desc, vsn} <- Application.started_applications(),
+        app = to_string(app),
+        String.starts_with?(app, query) and not List.starts_with?(desc, ~c"ERTS"),
+        into: %{},
+        do: {app, vsn}
+  end
+end
